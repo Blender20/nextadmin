d446f3322153cac38fcc46378603cc445b1eb9c1
Ray Yazdi <rayyazdi@outlook.com>
2023-11-09 20:54:24 -0500

    pathname and search params

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,11 +1,30 @@
+"use client"
 import { MdSearch } from 'react-icons/md'
 import styles from './search.module.css'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 const Search = ({ placeholder }) => {
+    const searchParams = useSearchParams();
+    const { replace } = useRouter()
+    const pathname = usePathname();
+
+    const handleSearch = (e) => {
+        searchParams.set("search", e.target.value)
+        replace(`${pathname}?${searchParams}`)
+    }
+
+    const params = new URLSearchParams(searchParams)
+
+    params.set("test", "value")
+
+    replace(`${pathname}?${params}`)
+    console.log(searchParams)
+    console.log(pathname)
+
     return (
         <div className={styles.container}>
             <MdSearch />
-            <input type="text" placeholder={placeholder} className={styles.input} />
+            <input type="text" placeholder={placeholder} className={styles.input} onChange={handleSearch} />
         </div>
     )
 }
